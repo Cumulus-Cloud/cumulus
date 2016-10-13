813a2663732ff0beff94d0660ea0ed43466cae53
Wadjetz <egor.neon@gmail.com>
2016-10-13 08:02:33 +0200

    Add PostCSS

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,5 +1,14 @@
-module.exports = {
-  entry: "./client/main.tsx",
+var webpack = require("webpack");
+var path = require("path");
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+var prod = process.env.NODE_ENV === "production";
+
+var config = {
+  entry: [
+    "./client/main.tsx",
+    "./client/styles/main.css"
+  ],
   output: {
     filename: "./public/main.js"
   },
@@ -15,6 +24,10 @@ module.exports = {
       {
         test: /\.tsx?$/,
         loader: "ts-loader"
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract("style", "css?-mergeRules!postcss")
       }
     ],
     preLoaders: [
@@ -23,5 +36,35 @@ module.exports = {
         loader: "source-map-loader"
       }
     ]
-  }
+  },
+  postcss: function () {
+    return [
+      require("postcss-import")({
+        from: path.resolve(__dirname, "./public/main.css"),
+        onImport: function (files) {
+          files.forEach(this.addDependency);
+        }.bind(this)
+      }),
+      require("postcss-nested")(),
+      require("postcss-cssnext")(),
+      require("css-mqpacker")(),
+      require("postcss-reporter")()
+    ];
+  },
+  plugins: [
+    new ExtractTextPlugin("./public/main.css"),
+    new webpack.ProvidePlugin({
+      "fetch": "imports?this=>global!exports?global.fetch!whatwg-fetch"
+    })
+  ]
 }
+
+if (!prod) {
+  config.devtool = "source-map";
+  config.plugins.push(new webpack.optimize.UglifyJsPlugin({
+    compressor: {screw_ie8: true, keep_fnames: false, warnings: false},
+    mangle: {screw_ie8: true, keep_fnames: false}
+  }));
+}
+
+module.exports = config;
