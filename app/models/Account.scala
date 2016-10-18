6179cb68cfd61b90f0e054480ce882d0bfbdd3b1
Vuzi <vuzi@free.fr>
2016-10-19 01:03:55 +0200

    Account id in exposed JSON

--- app/models/Account.scala
+++ app/models/Account.scala
@@ -47,11 +47,12 @@ object Account {
   )
 
   implicit val accountWrites: Writes[Account] = (
+    (JsPath \ "id").write[String] and
     (JsPath \ "mail").write[String] and
     (JsPath \ "login").write[String] and
     (JsPath \ "creation").write[DateTime] and
     (JsPath \ "roles").write[Seq[String]] and
     (JsPath \ "home").writeNullable[String]
-  )(account => (account.mail, account.login, account.creation, account.roles, account.home))
+  )(account => (account.id.toString, account.mail, account.login, account.creation, account.roles, account.home))
 
 }
