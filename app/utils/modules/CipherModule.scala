c2a21651dd9c59c2bd9ef74dc7df3623eac7c7bf
Vuzi <vuzi@free.fr>
2017-01-27 16:33:52 +0100

    cleanup

--- app/utils/modules/CipherModule.scala
+++
@@ -1,9 +0,0 @@
-package utils.modules
-
-import com.google.inject.AbstractModule
-
-class CipherModule extends AbstractModule {
-  override def configure(): Unit = {
-    println("WESHHHHHSHSHSH")
-  }
-}
