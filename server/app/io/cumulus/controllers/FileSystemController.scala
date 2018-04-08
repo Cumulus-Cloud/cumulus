4f7bda2205c4e308e2fb9e3c27f37cf5eb99d60c
Guillaume VILLEREZ <vuzi@free.fr>
2018-04-08 23:53:09 +0200

    New sharing API (#142)

--- server/app/io/cumulus/controllers/FileSystemController.scala
+++ server/app/io/cumulus/controllers/FileSystemController.scala
@@ -2,6 +2,8 @@ package io.cumulus.controllers
 
 import scala.concurrent.{ExecutionContext, Future}
 
+import akka.stream.scaladsl.Source
+import akka.util.ByteString
 import cats.data.EitherT
 import cats.implicits._
 import io.cumulus.controllers.payloads.fs._
@@ -16,7 +18,7 @@ import io.cumulus.models.{Path, Sharing, UserSession}
 import io.cumulus.persistence.services.{FsNodeService, SharingService, StorageService}
 import io.cumulus.stages._
 import play.api.libs.json.{JsString, Json}
-import play.api.mvc.{AbstractController, ControllerComponents}
+import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents}
 
 class FileSystemController(
   cc: ControllerComponents,
@@ -29,137 +31,195 @@ class FileSystemController(
   compressions: Compressions
 ) extends AbstractController(cc) with Authentication[UserSession] with ApiUtils with FileDownloaderUtils with BodyParserJson with BodyParserStream {
 
-  def get(path: Path, contentLimit: Option[Int], contentOffset: Option[Int]) = AuthenticatedAction.async { implicit request =>
-    ApiResponse {
-      fsNodeService.findNode(
-        path,
-        contentLimit
-          .map(QueryPagination(_, contentOffset))
-          .getOrElse(QueryPagination(51)) // Default pagination
-      )
-    }
-  }
-
-  def search(path: Path, name: String, nodeType: Option[FsNodeType], mimeType: Option[String], limit: Option[Int], offset: Option[Int]) = AuthenticatedAction.async { implicit request =>
-    ApiResponse {
-      fsNodeService.searchNodes(
-        path,
-        name,
-        nodeType,
-        mimeType,
-        limit
-          .map(QueryPagination(_, offset))
-          .getOrElse(QueryPagination(51)) // Default pagination
-      )
-    }
-  }
-
-  def download(path: Path, forceDownload: Option[Boolean]) = AuthenticatedAction.async { implicit request =>
-    ApiResponse.result {
-      for {
-        // Get the file
-        file       <- EitherT(fsNodeService.findFile(path))
-
-        // Get the file's content
-        maybeRange <- EitherT.fromEither[Future](headerRange(request, file))
-        content    <- EitherT.fromEither[Future](storageService.downloadFile(file, maybeRange))
-
-        // Create the response
-        result <- EitherT.pure[Future, AppError](
-          maybeRange match {
-            case Some(range) =>
-              streamFile(file, content, range)
-            case None =>
-              downloadFile(file, content, forceDownload.getOrElse(false))
-          }
+  /**
+    * Gets a filesystem element by its path.
+    * @param path The path of the element.
+    * @param contentLimit The maximum number of children elements (for a directory) to return. Used for pagination.
+    * @param contentOffset The offset of children elements (for a directory) to return. Used for pagination.
+    */
+  def get(path: Path, contentLimit: Option[Int], contentOffset: Option[Int]): Action[AnyContent] =
+    AuthenticatedAction.async { implicit request =>
+      ApiResponse {
+        fsNodeService.findNode(
+          path,
+          contentLimit
+            .map(QueryPagination(_, contentOffset))
+            .getOrElse(QueryPagination(51)) // Default pagination
         )
-      } yield result
+      }
     }
-  }
 
-  def downloadThumbnail(path: Path, forceDownload: Option[Boolean]) = AuthenticatedAction.async { implicit request =>
-    ApiResponse.result {
-      for {
-        // Get the file
-        file    <- EitherT(fsNodeService.findFile(path))
+  /**
+    * Search through the filesystem.
+    *
+    * @param path Root element for the search. Use '/' to search in the whole filesystem.
+    * @param name Name to look for. Approximation are allowed.
+    * @param nodeType The optional type of node to look for.
+    * @param mimeType The optional mime type to look for.
+    * @param limit The maximum number of children elements (for a directory) to return. Used for pagination.
+    * @param offset The offset of children elements (for a directory) to return. Used for pagination.
+    */
+  def search(path: Path, name: String, nodeType: Option[FsNodeType], mimeType: Option[String], limit: Option[Int], offset: Option[Int]): Action[AnyContent] =
+    AuthenticatedAction.async { implicit request =>
+      ApiResponse {
+        fsNodeService.searchNodes(
+          path,
+          name,
+          nodeType,
+          mimeType,
+          limit
+            .map(QueryPagination(_, offset))
+            .getOrElse(QueryPagination(51)) // Default pagination
+        )
+      }
+    }
 
-        // Get the file thumbnail's content
-        content <- EitherT(storageService.downloadThumbnail(path))
+  /**
+    * Downloads the file by its path.
+    *
+    * @param path The path of the file to download.
+    * @param forceDownload True to force the download, otherwise content will be opened directly in the browser.
+    */
+  def download(path: Path, forceDownload: Option[Boolean]): Action[AnyContent] =
+    AuthenticatedAction.async { implicit request =>
+      ApiResponse.result {
+        for {
+          // Get the file
+          file       <- EitherT(fsNodeService.findFile(path))
 
-        // Create the response
-        result  <- EitherT.pure[Future, AppError]{
-          file.thumbnailStorageReference match {
-            case Some(thumbnailStorageReference) =>
-              downloadFile(
-                s"thumbnail_${file.name}",
-                thumbnailStorageReference.size,
-                ThumbnailGenerator.thumbnailMimeType,
-                content,
-                forceDownload.getOrElse(false)
-              )
-            case _ =>
-              // Should never happen
-              toApiError(AppError.notFound("validation.fs-node.no-thumbnail", file.name))
+          // Get the file's content
+          maybeRange <- EitherT.fromEither[Future](headerRange(request, file))
+          content    <- EitherT.fromEither[Future](storageService.downloadFile(file, maybeRange))
+
+          // Create the response
+          result <- EitherT.pure[Future, AppError](
+            maybeRange match {
+              case Some(range) =>
+                streamFile(file, content, range)
+              case None =>
+                downloadFile(file, content, forceDownload.getOrElse(false))
+            }
+          )
+        } yield result
+      }
+    }
+
+  /**
+    * Download a file's thumbnail.
+    *
+    * @param path The path of the file.
+    * @param forceDownload True to force the download, otherwise content will be opened directly in the browser.
+    */
+  def downloadThumbnail(path: Path, forceDownload: Option[Boolean]): Action[AnyContent] =
+    AuthenticatedAction.async { implicit request =>
+      ApiResponse.result {
+        for {
+          // Get the file
+          file    <- EitherT(fsNodeService.findFile(path))
+
+          // Get the file thumbnail's content
+          content <- EitherT(storageService.downloadThumbnail(path))
+
+          // Create the response
+          result  <- EitherT.pure[Future, AppError]{
+            file.thumbnailStorageReference match {
+              case Some(thumbnailStorageReference) =>
+                downloadFile(
+                  s"thumbnail_${file.name}",
+                  thumbnailStorageReference.size,
+                  ThumbnailGenerator.thumbnailMimeType,
+                  content,
+                  forceDownload.getOrElse(false)
+                )
+              case _ =>
+                // Should never happen
+                toApiError(AppError.notFound("validation.fs-node.no-thumbnail", file.name))
+            }
           }
-        }
 
-      } yield result
+        } yield result
+      }
     }
-  }
 
- def upload(path: Path, cipherName: Option[String], compressionName: Option[String]) = AuthenticatedAction.async(streamBody) { implicit request =>
-   ApiResponse {
-     for {
-       // Get the cipher and compression from the request
-       cipher      <- EitherT.fromEither[Future](ciphers.get(cipherName))
-       compression <- EitherT.fromEither[Future](compressions.get(compressionName))
+  /**
+    * Upload a new file.
+    *
+    * @param path The path of the new file.
+    * @param cipherName The cipher to use.
+    * @param compressionName The compression to use.
+    */
+  def upload(path: Path, cipherName: Option[String], compressionName: Option[String]): Action[Source[ByteString, _]] =
+    AuthenticatedAction.async(streamBody) { implicit request =>
+      ApiResponse {
+        for {
+          // Get the cipher and compression from the request
+          cipher      <- EitherT.fromEither[Future](ciphers.get(cipherName))
+          compression <- EitherT.fromEither[Future](compressions.get(compressionName))
 
-       // Upload & create the file
-       file <- EitherT(storageService.uploadFile(path, cipher, compression, request.body))
+          // Upload & create the file
+          file <- EitherT(storageService.uploadFile(path, cipher, compression, request.body))
 
-     } yield file
-   }
- }
-
-  def create(path: Path) = AuthenticatedAction.async { implicit request =>
-    ApiResponse {
-      val directory = Directory.create(request.user.id, path)
-      fsNodeService.createDirectory(directory)
+        } yield file
+      }
     }
-  }
 
-  def update(path: Path) = AuthenticatedAction.async(parseJson[FsOperation]) { implicit request =>
-    request.body match {
-      case FsOperationCreate(_) =>
+  /**
+    * Create a new directory.
+    *
+    * @param path The path of the new directory.
+    */
+  def create(path: Path): Action[AnyContent] =
+    AuthenticatedAction.async { implicit request =>
+      ApiResponse {
         val directory = Directory.create(request.user.id, path)
-        ApiResponse(fsNodeService.createDirectory(directory))
-      case FsOperationMove(to) =>
-        ApiResponse(fsNodeService.moveNode(path, to))
-      case FsOperationShareLink(_, duration, _) =>
-        ApiResponse {
-          sharingService.shareNode(path, request.user.password, duration).map {
-            case Right((sharing, secretCode)) =>
-              Right(Json.toJsObject(sharing)(Sharing.apiWrite)
-                + ("key" -> Json.toJson(secretCode))
-                + ("download" -> JsString(routes.SharingController.downloadRoot(sharing.reference, path.name, secretCode, None).url))
-                + ("path" -> JsString(routes.SharingController.get("/", sharing.reference, secretCode).url))
-              )
-            case Left(e) =>
-              Left(e)
-          }
-        }
-      case FsOperationShareDelete(reference) =>
-        ApiResponse(sharingService.deleteSharing(reference))
-      case FsOperationDelete(_) =>
-        ApiResponse(storageService.deleteNode(path))
+        fsNodeService.createDirectory(directory)
+      }
     }
-  }
 
-  def delete(path: Path) = AuthenticatedAction.async { implicit request =>
-    ApiResponse {
-      storageService.deleteNode(path)
+  /**
+    * Update a file.
+    *
+    * @param path The file to update.
+    */
+  def update(path: Path): Action[FsOperation] =
+    AuthenticatedAction.async(parseJson[FsOperation]) { implicit request =>
+      request.body match {
+        case FsOperationCreate(_) =>
+          val directory = Directory.create(request.user.id, path)
+          ApiResponse(fsNodeService.createDirectory(directory))
+        case FsOperationMove(to) =>
+          ApiResponse(fsNodeService.moveNode(path, to))
+        case FsOperationShareLink(_, duration, _) =>
+          ApiResponse {
+            sharingService.shareNode(path, request.user.password, duration).map {
+              case Right((sharing, secretCode)) =>
+                Right(Json.toJsObject(sharing)(Sharing.apiWrite)
+                  + ("key" -> Json.toJson(secretCode))
+                  + ("download" -> JsString(routes.SharingController.downloadRoot(sharing.reference, path.name, secretCode, None).url))
+                  + ("path" -> JsString(routes.SharingController.get("/", sharing.reference, secretCode).url))
+                )
+              case Left(e) =>
+                Left(e)
+            }
+          }
+        case FsOperationShareDelete(reference) =>
+          ApiResponse(sharingService.deleteSharing(reference))
+        case FsOperationDelete(_) =>
+          ApiResponse(storageService.deleteNode(path))
+      }
     }
-  }
+
+  /**
+    * Delete a file by its path.
+    *
+    * @param path The path of the file to delete.
+    */
+  def delete(path: Path): Action[AnyContent] =
+    AuthenticatedAction.async { implicit request =>
+      ApiResponse {
+        storageService.deleteNode(path)
+      }
+    }
 
 }
 
