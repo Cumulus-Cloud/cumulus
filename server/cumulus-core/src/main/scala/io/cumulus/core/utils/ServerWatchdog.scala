60872fb02502c1af15c69f829e9f501977021c3d
Guillaume VILLEREZ <vuzi@free.fr>
2018-05-01 03:11:38 +0200

    Cleanup #4

--- server/cumulus-core/src/main/scala/io/cumulus/core/utils/ServerWatchdog.scala
+++ server/cumulus-core/src/main/scala/io/cumulus/core/utils/ServerWatchdog.scala
@@ -1,11 +1,17 @@
 package io.cumulus.core.utils
 
+/**
+  * Common trait for the server watchdog.
+  */
 trait ServerWatchdog {
 
+  /** Start the main server. Should have no effect if the server is already started. */
   def start(): Unit
 
+  /** Stop the server and the scala application. */
   def stop(): Unit
 
+  /** Reload the server. */
   def reload(): Unit
 
 }
