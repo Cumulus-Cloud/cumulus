981ce3ad4f906fc308a03dba1332121ecf6bb4cd
Egor Berezovskiy <egor.neon@gmail.com>
2018-05-19 12:05:51 +0200

    Upgrade rxjs to version 6 (#206)

--- client/src/files/newFolder/NewFolderEpics.ts
+++ client/src/files/newFolder/NewFolderEpics.ts
@@ -1,6 +1,7 @@
-import { isActionOf } from "typesafe-actions"
-import { Observable } from "rxjs/Observable"
 import { Epic, combineEpics } from "redux-observable"
+import { isActionOf } from "typesafe-actions"
+import { of } from "rxjs"
+import { filter, mergeMap, map, catchError } from "rxjs/operators"
 import { GlobalState, Dependencies } from "store"
 import { Actions } from "actions"
 import { showApiErrorNotif } from "inAppNotif/InAppNotifActions"
@@ -9,21 +10,18 @@ import { ApiError } from "models/ApiError"
 
 type EpicType = Epic<Actions, GlobalState, Dependencies>
 
-export const createNewFolderEpic: EpicType = (action$, _, dependencies) => {
-  return action$
-    .filter(isActionOf(NewFolderActions.createNewFolder))
-    .mergeMap(({ payload: { currentDirectory, newFolderName } }) =>
-      dependencies.requests.createFnNode(currentDirectory, newFolderName, "DIRECTORY")
-        .map(newFolder => NewFolderActions.createNewFolderSuccess({ newFolder }))
-        .catch((error: ApiError) => Observable.of(NewFolderActions.createNewFolderError({ error })))
-    )
-}
+export const createNewFolderEpic: EpicType = (action$, _, { requests }) => action$.pipe(
+  filter(isActionOf(NewFolderActions.createNewFolder)),
+  mergeMap(({ payload: { currentDirectory, newFolderName } }) => requests.createFnNode(currentDirectory, newFolderName, "DIRECTORY").pipe(
+    map(newFolder => NewFolderActions.createNewFolderSuccess({ newFolder })),
+    catchError((error: ApiError) => of(NewFolderActions.createNewFolderError({ error })))
+  ))
+)
 
-export const createNewFolderErrorEpic: EpicType = action$ => {
-  return action$
-    .filter(isActionOf(NewFolderActions.createNewFolderError))
-    .map(({ payload: { error } }) => showApiErrorNotif(error))
-}
+export const createNewFolderErrorEpic: EpicType = action$ => action$.pipe(
+  filter(isActionOf(NewFolderActions.createNewFolderError)),
+  map(({ payload: { error } }) => showApiErrorNotif(error))
+)
 
 export const createNewFolderEpics = combineEpics(
   createNewFolderEpic, createNewFolderErrorEpic,
