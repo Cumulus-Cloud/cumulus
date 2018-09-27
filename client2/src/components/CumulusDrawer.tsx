cfdf3dd320b5b077d44884db50c80b9c1c3203cb
Guillaume VILLEREZ <vuzi@free.fr>
2018-09-28 00:08:21 +0200

    Mobile drawer fix

--- client2/src/components/CumulusDrawer.tsx
+++ client2/src/components/CumulusDrawer.tsx
@@ -5,8 +5,10 @@ import List from '@material-ui/core/List'
 import { Theme } from '@material-ui/core/styles/createMuiTheme'
 import createStyles from '@material-ui/core/styles/createStyles'
 import withStyles, { WithStyles } from '@material-ui/core/styles/withStyles'
+import IconButton from '@material-ui/core/IconButton'
 import SwipeableDrawer from '@material-ui/core/SwipeableDrawer'
 import CloudIcon from '@material-ui/icons/CloudQueue'
+import MenuIcon from '@material-ui/icons/Menu'
 import { Typography } from '@material-ui/core'
 
 
@@ -15,6 +17,16 @@ const styles = (theme: Theme) => createStyles({
     backgroundColor: 'rgb(246, 251, 251)', // TODO via theme
     position: 'relative',
     width: 240,
+  },
+  drawerBar: {
+    backgroundColor: 'rgb(246, 251, 251)', // TODO via theme
+    position: 'relative',
+    width: 48,
+    height: '100%',
+    borderRight: '1px solid rgba(0, 0, 0, 0.12)',
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
+    }
   },
   drawerStatic: {
     [theme.breakpoints.down('sm')]: {
@@ -63,27 +75,35 @@ class CumulusDrawer extends React.Component<PropsWithStyle, State> {
   }
 
   render() {
-    const { classes, searchElements, actionElements, contextualActionElements, showDynamicDrawer } = this.props
+    const { classes, actionElements, contextualActionElements, showDynamicDrawer } = this.props
 
     return (
       <div>
         <SwipeableDrawer
           open={showDynamicDrawer}
+          classes={{ paper: classes.drawerPaper }}
           onClose={() => this.toggleDrawer()}
           onOpen={() => this.toggleDrawer()}
         >
-          <List>{searchElements}</List>
+          <div className={classes.logoContainer} >
+            <Typography variant="headline" className={classes.logoText} >
+              <CloudIcon  className={classes.logo} /> <div>Cumulus</div>
+            </Typography>
+          </div>
           <Divider style={{height: 1}} />
           <List>{actionElements}</List>
           <Divider style={{height: 1}} />
           <List>{contextualActionElements}</List>
         </SwipeableDrawer>
+        <div className={classes.drawerBar} >
+          <IconButton onClick={() => this.toggleDrawer()} >
+            <MenuIcon />
+          </IconButton>
+        </div>
         <Drawer
           variant="permanent"
           className={classes.drawerStatic}
-          classes={{
-            paper: classes.drawerPaper,
-          }}
+          classes={{ paper: classes.drawerPaper }}
         >
           <div className={classes.logoContainer} >
             <Typography variant="headline" className={classes.logoText} >
@@ -98,8 +118,7 @@ class CumulusDrawer extends React.Component<PropsWithStyle, State> {
       </div>
     )
   }
-
-  // <List>{searchElements}</List>
+  
 }
 
 export default withStyles(styles) <PropsWithStyle> (CumulusDrawer)
